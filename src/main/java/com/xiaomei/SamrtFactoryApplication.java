3566ab1132cd0049b49092247350060d4c464a57
Mei Sure <geshuchen709@gmail.com>
2026-06-30 09:09:01 +0800

    搭建springFramework

---
+++ src/main/java/com/xiaomei/SamrtFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.xiaomei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamrtFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamrtFactoryApplication.class, args);
+	}
+
+}
